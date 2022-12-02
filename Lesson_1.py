7d331fe1d84dd5b213612a17c1cfec93c7103d8f
FIRST_NAME LAST_NAME <gal.ilmir@mail.ru>
2022-12-02 15:37:26 +0300

    Lesson 1

---
+++ Lesson_1.py
@@ -0,0 +1,36 @@
+#a = 123
+#b = 1.23
+#print (a) 
+#s = "hello boss"
+#s = "hello \nboss" #перенос слова на новую строку
+#print (s) 
+#f = True
+#list = [1,2,3, s]
+#print (list)
+#print ("введите имя a")
+#a = input()
+#print ("введите имя b")
+#b = input()
+#print (a, b)
+#a = 5
+#b = 3
+#c = a / b
+#print (c)
+
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print ("большее число", a)
+# else:
+#     print ("большее число", b)
+
+# a = input("сколько вам лет?  ")
+# if a == 17:
+#     print ('вам вход запрещен') не получилось
+
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted=inverted*10+(original%10)
+#     original//=10
+# print(inverted)
---
+++ Seminar_1.py
@@ -0,0 +1,51 @@
+# 1. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+# print('Введите первое число')
+# first_namber = int(input())
+# print('Введите второе число')
+# second_namber = int(input())
+# if first_namber*first_namber == second_namber or second_namber*second_namber == first_namber:
+#     print('число является квадратом другого')
+# else:
+#     print('число не является квадратом другого')
+
+
+# 2. Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+# print('Введите первое число')
+# a = int(input())
+# print('Введите первое число')
+# b = int(input())
+# print('Введите первое число')
+# c = int(input())
+# print('Введите первое число')
+# d = int(input())
+# print('Введите первое число')
+# e = int(input())
+# max_num = a
+# if a > max_num:
+#     max_num = a
+# if b > max_num:
+#     max_num = b
+# if c > max_num:
+#     max_num = c
+# if d > max_num:
+#     max_num = c
+# if e > max_num:
+#     max_num = e
+# print(max_num)    
+
+
+
+
+# 1. Напишите программу, которая будет на вход принимать число N и выводить числа от -N до N
+# n = int(input('Введите число:\n'))
+# n_reverst = n*-1
+# print(n_reverst)
+# for num in range(n_reverst, n):
+#     n_reverst += 1
+#     print(n_reverst)
+
+
+# 2. Напишите программу, которая будет принимать на вход вещественное число и показывать первую цифру дробной части числа.
+n = float(input('Введите вещественное число:\n'))
+h = n % 10
+print(hg)
